eb9c0848fe506cd0fd06bf8d541ecfbc9232da25
강민규 <GithubKangMin@users.noreply.github.com>
2025-09-09 16:29:12 +0900

    프록시 요청을 프록시 요청 헤더에 credentials를 설정하도록 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,11 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        credentials: 'include'
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('credentials', 'include');
+          });
+        }
       }
     }
   },
